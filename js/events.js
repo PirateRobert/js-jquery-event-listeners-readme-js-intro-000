2e3caff26a3659c1912e977a8f56b521984de421
This is Rude <dbauman1@swarthmore.edu>
2018-01-07 05:06:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,11 @@ function frameIt(){
   $("img").on("")
 }
 function pressIt(){
-  $("input").on("")
+  $("input").on("keydown",function(key){
+    if(key.which === 71){
+      alert("G-League...now on Twitch.tv")
+    }
+  })
 }
 $(document).ready(function(){
 
